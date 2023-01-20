b90a816134ffaefdd04f99854aee6c1e475087a1
Srikaran Devarakonda <114800064+srikaran3004@users.noreply.github.com>
2023-01-20 08:28:24 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,38 @@
+// Two types of Data Types are there 
+// 1)Primitive
+// in this we have 
+// Integer --- byte,short,int,long
+// Float-- double 8 bytes ,float 4 bytes
+// Charecters 2 bytes
+// Boolean
+
+// Integer can be stored as 
+// int is 4 bytes
+// long is 8 bytes 
+// short is 2 bytes 
+// byte is -127 to 128
+
+class DataTypes
+{
+    public static void main(String a[])
+    {
+        // int num1 = 9;
+        // byte by= 127;
+        // short sh = 558;
+        // long l = 5854l;
+        // float marks = 5.8f;
+        // double d=5.8;
+        // char c='k';
+        // boolean b = true;
+        // Generally in java point values it considers as double values to make a double value into a float explicitly mention the number ending by "f" for ex: double num = 5.6 is okay , but float num = 5.6 is not accepted so inorder to make double to float we write it as float num = 5.6f; 
+
+
+    // Literals *****
+        // int num1=1000_000;
+        // double d=12e10;
+        char c = 'a';
+        c++;
+        // Here we are adding c++ so that the given char value gets incremented by one number 
+        System.out.println(c);
+    }
+}
